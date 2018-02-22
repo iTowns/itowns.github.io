6eaca1e5d5af4201c1e70ae52f783d4cb511f01e
Augustin Trancart <augustin.trancart@oslandia.com>
2018-02-22 09:50:31 +0100

    Support different site.baseurl
    When forking this repo and activating gh pages, the base url is
    <username>.github.io/itowns.github.io and all the links are broken. This
    commit correctly uses site.baseurl to allow live testing of this repo.

--- js/init.js
+++ js/init.js
@@ -9,12 +9,12 @@
     skel.init({
         reset: 'full',
         breakpoints: {
-            global:		{ range: '*', href: '/css/style.css', containers: 1400, grid: { gutters: 50 } },
-            wide:		{ range: '-1680', href: '/css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
-            normal:		{ range: '-1280', href: '/css/style-normal.css', containers: 960, viewport: { scalable: false } },
-            narrow:		{ range: '-980', href: '/css/style-narrow.css', containers: '95%', grid: { gutters: 30 } },
-            narrower:	{ range: '-840', href: '/css/style-narrower.css', grid: { collapse: 1 } },
-            mobile:		{ range: '-736', href: '/css/style-mobile.css', containers: '100%', grid: { gutters: 15, collapse: 2 } }
+            global:		{ range: '*', href: baseUrl + '/css/style.css', containers: 1400, grid: { gutters: 50 } },
+            wide:		{ range: '-1680', href: baseUrl + '/css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
+            normal:		{ range: '-1280', href: baseUrl + '/css/style-normal.css', containers: 960, viewport: { scalable: false } },
+            narrow:		{ range: '-980', href: baseUrl + '/css/style-narrow.css', containers: '95%', grid: { gutters: 30 } },
+            narrower:	{ range: '-840', href: baseUrl + '/css/style-narrower.css', grid: { collapse: 1 } },
+            mobile:		{ range: '-736', href: baseUrl + '/css/style-mobile.css', containers: '100%', grid: { gutters: 15, collapse: 2 } }
         },
         plugins: {
             layers: {
